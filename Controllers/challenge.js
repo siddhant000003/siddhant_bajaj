630124ef8509dfe3147e1ecd16e2c9501590f931
siddhant000003 <109249227+siddhant000003@users.noreply.github.com>
2022-07-18 10:59:41 +0530

    Add files via upload

---
+++ Controllers/challenge.js
@@ -0,0 +1,29 @@
+function charIsLetter(char) {
+  if (typeof char !== "string") {
+    return false;
+  }
+
+  return /^[a-zA-Z]+$/.test(char);
+}
+
+exports.getData = async (req, res, next) => {
+  try {
+    data = await req.body;
+    const numberArray = data.data.filter((item) => +item);
+    const alphabetArray = data.data.filter((item) => charIsLetter(item));
+    const totalCount = numberArray.length + alphabetArray.length;
+    res.status(200).json({
+      is_success: true,
+      count: totalCount.toString(),
+      user_id: "Siddhant_Bhalla ",
+      email_id: "siddhant.bhalla.btech2019@sitpune.edu.in",
+      roll_number: "19070124062",
+      numbers: numberArray,
+      alphabets: alphabetArray,
+    });
+  } catch (error) {
+    res.json({ is_success: false });
+    error.statusCode = 500;
+    throw error;
+  }
+};
