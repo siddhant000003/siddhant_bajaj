a7e29270caaad1019a05a27d05ea4575d41cb220
siddhant000003 <109249227+siddhant000003@users.noreply.github.com>
2022-07-18 11:14:23 +0530

    Delete Routes directory

--- Routes/challenge.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const challengeController = require("../Controllers/challenge");
-
-router.post("/challenge", challengeController.getData);
-
-module.exports = router;
